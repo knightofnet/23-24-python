cd82a30c6d9b1a476ceea7b67be8570f38354546
Aryx <39461001+knightofnet@users.noreply.github.com>
2023-11-27 15:02:41 +0100

    Commentaires TP10.py

--- TP/tp10/exo1.py
+++ TP/tp10/exo1.py
@@ -1,62 +1,78 @@
-class Chien :
-    def __init__(self, pIdentification, pNom, pRace, pDatenaiss, pTaille=0, pPoids=0) :
-        self.identification = pIdentification
-        self.nom = pNom        
-        self.race = pRace      
-        self.dateNaiss = pDatenaiss
-        self.taille = pTaille  
-        self.poids = pPoids 
+# Définition de la classe Chien
+class Chien:
+    # Méthode d'initialisation (constructeur) de la classe Chien
+    def __init__(self, pIdentification, pNom, pRace, pDatenaiss, pTaille=0, pPoids=0):
+        # Initialisation des attributs de l'instance
+        self.identification = pIdentification  # Identifiant unique du chien
+        self.nom = pNom                         # Nom du chien
+        self.race = pRace                       # Race du chien
+        self.dateNaiss = pDatenaiss             # Date de naissance du chien (tuple)
+        self.taille = pTaille                   # Taille du chien en cm
+        self.poids = pPoids                     # Poids du chien en kg
 
-    def affiche(self) :
+    # Méthode pour afficher les informations d'un chien
+    def affiche(self):
         print(f"{self.nom} est un chien de race {self.race}")
+        # Affichage formaté de la date de naissance
         print(f"Il est né le {self.dateNaiss[0]} / {self.dateNaiss[1]} / {self.dateNaiss[2]}")
         print(f"Il mesure {self.taille} cm")
         
-        if (self.poids > 0) :
-            print(f"Il pèse {self.poids} cm")
+        # Condition pour vérifier si le poids est spécifié
+        if self.poids > 0:
+            print(f"Il pèse {self.poids} kg")
 
-        if ("P" == self.identification[0]) :
+        # Condition pour vérifier le type d'identification (puce ou tatouage)
+        if "P" == self.identification[0]:
             print(f"Il a une puce d'identification : {self.identification[1:]}")
-        elif ("T" == self.identification[0]) :
+        elif "T" == self.identification[0]:
             print(f"Il est tatoué : {self.identification[1:]}")
 
-class Candidat(Chien) :
-
+# Définition de la classe Candidat qui hérite de la classe Chien
+class Candidat(Chien):
+    # Attribut de classe pour conserver la liste des concours connus
     lstConcoursConnus = []
 
+    # Méthode d'initialisation (constructeur) de la classe Candidat
     def __init__(self, pIdentification, pNom, pRace, pDatenaiss, pTaille=0, pPoids=0, concours=[]):
-        Chien.__init__(self, pIdentification, pNom, pRace, pDatenaiss, pTaille, pPoids)
-        self.concours = concours
+        # Appel au constructeur de la classe parent (Chien)
+        super().__init__(pIdentification, pNom, pRace, pDatenaiss, pTaille, pPoids)
+        self.concours = concours  # Liste des concours auxquels le chien a participé
 
+        # Mise à jour de la liste des concours connus
         Candidat.lstConcoursConnus.extend(concours)
         Candidat.lstConcoursConnus = list(set(Candidat.lstConcoursConnus))
 
+    # Méthode pour afficher les informations du candidat
     def affiche(self):
+        # Appel à la méthode affiche de la classe parent (Chien)
         super().affiche()
         print(f"il a participé à {len(self.concours)} concours :")
-        for concour in self.concours :
+        for concour in self.concours:
             print(f"    {concour}")
 
-    def participe(self, nomConcour) :
+    # Méthode pour ajouter un nouveau concours à la liste du candidat
+    def participe(self, nomConcour):
         self.concours.append(nomConcour) 
         Candidat.lstConcoursConnus.append(nomConcour)
         Candidat.lstConcoursConnus = list(set(Candidat.lstConcoursConnus))
 
+    # Méthode de classe pour afficher tous les concours connus
     @classmethod
-    def concoursConnus(cls) :
+    def concoursConnus(cls):
         print("Voici les concours que nous avons répertoriés:", cls.lstConcoursConnus)
 
-c1 = Chien("P250263890203125","Tara","Bichon",(2,5,2022),25.5)
-c2 = Chien("TAG896","Sophia","Berger",(3,6,2021),48,5.6)
+# Création d'instances de la classe Chien et utilisation des méthodes
+c1 = Chien("P250263890203125", "Tara", "Bichon", (2, 5, 2022), 25.5)
+c2 = Chien("TAG896", "Sophia", "Berger", (3, 6, 2021), 48, 5.6)
 
-# c1.affiche()
-# c2.affiche()
-
+# Création d'instances de la classe Candidat, sous-classe de Chien
 cand1 = Candidat("P250261992073142","Sangdor","Caniche",(12,6,2021),31.5,3.1,["Tours canin 2022","Mon chien 2022"])
+# Fais participer le candidat Sangdaor, cand1, au concours : Concours canin Langeais 2022
 cand1.participe("Concours canin Langeais 2022")
+
 cand1.affiche()
 
 cand2 = Candidat("P250261920341256","Tador","Caniche",(1,5,2022),28.5,2.9,["Mon chien 2022","os d'or 2022"])
 
+# Affichage des concours connus
 Candidat.concoursConnus()
-
--- TP/tp10/exo2_b.py
+++ TP/tp10/exo2_b.py
@@ -1,33 +1,50 @@
+# Définition de la classe Inventaire
 class Inventaire():
     
-    def __init__(self, nom, inventaire={}) :
-        self.nom = nom
-        self.inventaire = inventaire
+    # Constructeur de la classe avec deux paramètres : nom et inventaire
+    def __init__(self, nom, inventaire={}):
+        self.nom = nom  # Nom de l'inventaire
+        self.inventaire = inventaire  # Dictionnaire représentant l'inventaire (clé: article, valeur: poids)
 
-    def __str__(self) :
+    # Méthode spéciale pour définir la représentation en chaîne de caractères de l'objet Inventaire
+    def __str__(self):
+        # Obtention et tri des clés du dictionnaire inventaire
         lstClefs = list(self.inventaire.keys())
         lstClefs.sort()
 
+        # Construction d'une liste de chaînes de caractères représentant chaque article et son poids
         retour = []
-        for clef in lstClefs :
+        for clef in lstClefs:
             chaine = f"{clef} : {self.inventaire[clef]} kg"
             retour.append(chaine)
 
-        return f"Inventaire {self.nom}\n" +  "\n".join(retour)
+        # Assemblage des chaînes de caractères en une seule, séparées par des retours à la ligne
+        return f"Inventaire {self.nom}\n" + "\n".join(retour)
 
-    def __add__(self, o) :
-
+    # Méthode spéciale pour surcharger l'opérateur '+'
+    def __add__(self, o):
+        # Création d'un nouveau nom pour l'inventaire combiné
         nouveauNom = f"{self.nom} + {o.nom}"
-        dicoNouvelInventaire = {}
-        clefs = list( set( list(self.inventaire.keys()) + list(o.inventaire.keys()) )   )
 
-        for clef in clefs :
+        # Initialisation d'un nouveau dictionnaire pour le nouvel inventaire
+        dicoNouvelInventaire = {}
+
+        # Création d'une liste unique des clés des deux inventaires
+        clefs = list(set(list(self.inventaire.keys()) + list(o.inventaire.keys())))
+
+        # Calcul des sommes des poids pour chaque article des deux inventaires
+        for clef in clefs:
             dicoNouvelInventaire[clef] = self.inventaire.get(clef, 0) + o.inventaire.get(clef, 0)
 
+        # Retour d'un nouvel objet Inventaire combinant les deux inventaires
         return Inventaire(nouveauNom, dicoNouvelInventaire)
 
-inv1 = Inventaire("magasin 1", {'pâtes' : 2 , 'farine': 1 })
-inv2 = Inventaire("magasin 2", {'pâtes' : 2 , 'lessive' : 2})
+# Création d'instances de la classe Inventaire
+inv1 = Inventaire("magasin 1", {'pâtes': 2, 'farine': 1})
+inv2 = Inventaire("magasin 2", {'pâtes': 2, 'lessive': 2})
 
+# Utilisation de la surcharge de l'opérateur '+' pour combiner deux inventaires
 inv3 = inv1 + inv2
+
+# Affichage du nouvel inventaire combiné
 print(inv3)
